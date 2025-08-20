0d69e6a82c41671a1a150aaf7f6be80907f867ef
Anderson Vieira <asvieira.dev@gmail.com>
2025-08-20 16:08:33 -0300

    feat: classes AdminUser, EditorUser and GuessUser extends from abstract class User

--- src/main/java/cms/user/EditorUser.java
+++ src/main/java/cms/user/EditorUser.java
@@ -1,32 +1,8 @@
 package cms.user;
 
-public class EditorUser {
-	private final int id;
-	private final String nome;
-	private final String email;
-	private final String password;
-
+public class EditorUser extends User {
 	public EditorUser(int id, String nome, String email, String password) {
-		this.id = id;
-		this.nome = nome;
-		this.email = email;
-		this.password = password;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public boolean checkPassword(String p) {
-		return password.equals(p);
+		super(id, nome, email, password);
 	}
 
 	public void editContent(int contentId, String body) {
